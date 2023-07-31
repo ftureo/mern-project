aab22eb8b642aa97c93ebf38ee74792571b4f7c6
Fabi Tureo <fabianestebantureo@gmail.com>
2023-07-31 13:55:37 -0300

    Feat: :sparkles: Add upload images through cloudinary and remove files after upload

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,19 +1,29 @@
-import { uploadImageToCloudinary } from "../utils/cloudinary.js"
+import fs from "fs-extra";
+
+import { uploadImageToCloudinary } from "../utils/cloudinary.js";
 
 export const getBooks = (request, response) => {
-    response.send("This route will show all books")
-}
+    response.send("This route will show all books");
+};
 
 export const createBook = async (request, response) => {
-    console.log({ body: request.body })
-    console.log({ files: request.files }) // express-fileupload
+    console.log({ body: request.body });
+    console.log({ files: request.files }); // express-fileupload
 
-    const { bookName, author } = request.body
+    const { bookName, author } = request.body;
+    let image;
 
-    if(request.files?.bookImage) {
-        const { bookImage } = request.files
-        const resultUpload = await uploadImageToCloudinary(bookImage.tempFilePath)
-        console.log({ resultUpload })
+    if (request.files?.bookImage) {
+        const { bookImage } = request.files;
+        const resultUpload = await uploadImageToCloudinary(
+            bookImage.tempFilePath
+        );
+        image = {
+            urlImage: resultUpload?.secure_url,
+            publicId: resultUpload?.public_id,
+        };
+        await fs.remove(bookImage.tempFilePath)
+        console.log({ resultUpload });
     }
 
     response.send({
@@ -21,16 +31,17 @@ export const createBook = async (request, response) => {
         newBook: {
             title: bookName,
             author,
-        }
-    })
-}
+            image,
+        },
+    });
+};
 
 export const deleteBook = (request, response) => {
-    console.log({ params: request.params })
+    console.log({ params: request.params });
 
-    const { bookId } = request.params
+    const { bookId } = request.params;
 
     response.send({
         message: `Book with id ${bookId} deleted successfully`,
-    })
-}
+    });
+};
