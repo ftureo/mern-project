5df7f6f130938849bff9ce967fd56b309d654672
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-01 17:38:00 -0300

    Feat: ✨ Add methods in bookController

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,15 +1,18 @@
 import fs from "fs-extra";
 
-import { uploadImageToCloudinary } from "../utils/cloudinary.js";
+import { uploadImageToCloudinary, deleteImageFromCloudinary } from "../utils/cloudinary.js";
+import Book from "../models/book.model.js";
 
-export const getBooks = (request, response) => {
-    response.send("This route will show all books");
+export const getBooks = async (request, response) => {
+    const books = await Book.find()
+
+    response.send({
+        message: "Books retrieved successfully",
+        books
+    })
 };
 
 export const createBook = async (request, response) => {
-    console.log({ body: request.body });
-    console.log({ files: request.files }); // express-fileupload
-
     const { bookName, author } = request.body;
     let image;
 
@@ -22,26 +25,49 @@ export const createBook = async (request, response) => {
             urlImage: resultUpload?.secure_url,
             publicId: resultUpload?.public_id,
         };
-        await fs.remove(bookImage.tempFilePath)
+        await fs.remove(bookImage.tempFilePath);
         console.log({ resultUpload });
     }
 
+    const newBook = new Book({
+        bookName,
+        author,
+        image,
+    });
+
+    const newBookSaved = await newBook.save();
+
     response.send({
         message: "Book created successfully",
-        newBook: {
-            title: bookName,
-            author,
-            image,
-        },
+        id: newBookSaved._id,
+        title: newBookSaved.bookName,
+        author: newBookSaved.author,
+        image: newBookSaved.image,
     });
 };
 
-export const deleteBook = (request, response) => {
-    console.log({ params: request.params });
-
+export const deleteBook = async (request, response) => {
     const { bookId } = request.params;
+    console.log({ bookId });
 
-    response.send({
-        message: `Book with id ${bookId} deleted successfully`,
-    });
+    try {
+        const removedBook = await Book.findByIdAndDelete(bookId);
+        if(!removedBook) return response.status(404).send({ message: `Book with id ${bookId} not was found` });
+
+        console.log({ removedBook });
+
+        if(removedBook?.image?.publicId){
+            await deleteImageFromCloudinary(removedBook.image.publicId);
+        }
+
+        response.status(204).send({
+            message: `Book with id ${bookId} deleted successfully`,
+            removedBook
+        })
+
+
+    } catch (error){
+        console.log(error);
+        throw new Error(`Error deleting book with id ${bookId}`);
+    }
 };
