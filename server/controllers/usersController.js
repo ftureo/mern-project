e5ca4a7604baa73d8700a2c83f1ebcd2e832aa1f
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-07 19:39:03 -0300

    Feat: ✨ Add methods for users in controller

---
+++ server/controllers/usersController.js
@@ -0,0 +1,7 @@
+import User from "../models/user.model.js"
+
+export const getUsers = async (request, response) => {}
+
+export const register = async (request, response) => {}
+
+export const login = async (request, response) => {}
