f2c7b1cd422120cc30e2748218ca4196ea44390d
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-11 23:59:47 -0300

    Feat: ✨ Add finder by userId

--- server/routes/books.routes.js
+++ server/routes/books.routes.js
@@ -6,6 +6,7 @@ import {
     deleteBook,
     getBookById,
     updateBook,
+    getBooksByUser
 } from "../controllers/booksController.js";
 import { verifyAccessToken } from '../middlewares/verifyToken.js';
 
@@ -13,6 +14,7 @@ const router = Router();
 
 router.get("/books", getBooks)
 router.get("/get-book/:bookId", getBookById)
+router.get("/get-books-by-user", verifyAccessToken, getBooksByUser)
 router.post("/create-book", verifyAccessToken, createBook)
 router.put("/update-book/:bookId", verifyAccessToken, updateBook)
 router.delete("/delete-book/:bookId", verifyAccessToken, deleteBook)
