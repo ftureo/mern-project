f65a29425d04dc0efa06b50ba7040f5f95f93641
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-12 01:56:44 -0300

    Feat: 😍 Client integration

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <>
+            <div className="container bg-gray-800">
+                <p className="text-4xl font-boldnom">
+                    Click on the Vite and React logos to learn more
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default App;
