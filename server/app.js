494c5d5dfab3ead562c008a411ed5e398e463489
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-11 21:44:11 -0300

    Feat: ✨ Add cookie-parser middleware

--- server/app.js
+++ server/app.js
@@ -7,10 +7,12 @@ import testRoutes from "./routes/test.routes.js";
 import booksRoutes from "./routes/books.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { createRoles } from "./utils/roles-initializer.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cookieParser());
 app.use(
     fileUpload({
         useTempFiles: true,
