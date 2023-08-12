872cf828d13210008e186b0c680fd430253e8799
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-11 23:49:14 -0300

    Feat: ✨ Add user creator in body request

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,23 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const verifyAccessToken = async (request, response, next) => {
-    console.log("Viendo las cookies", request.cookies);
     const token = request.cookies.access_token;
 
     if (!token) {
         return response.status(401).json({ message: "Access token not found" });
     }
-
-    console.log("token", token)
-
     try {
         const decodeUser = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, {
             expiresIn: "84600",
-        });
-
-        console.log("decodeUser", decodeUser);
-
-        request.userId = decodeUser.id;
+        }); 
+        
+        request.body.userCreatorId = decodeUser.id;
         next();
     } catch (error) {
         return response.status(403).json({ message: "Invalid token" });
