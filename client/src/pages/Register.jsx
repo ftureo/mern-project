8d7d296aa6f75288e096109c3afa82e6164a0bdf
Fabi Tureo <fabianestebantureo@gmail.com>
2023-08-12 11:58:50 -0300

    Feat: ✨ Add register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,12 @@
 export const Register = () => {
     return (
         <div>
-            Register
+            <form action=""> 
+                <input type="text" placeholder="Nombre" name="username"/>
+                <input type="text" placeholder="Apellido" name="lastname"/>
+                <input type="email" placeholder="Email" name="email"/>
+                <input type="password" placeholder="Contraseña" name="password"/>
+            </form>
         </div>
     )
 }
